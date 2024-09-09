d3d2388c337bf92821ac2411dab4b3b5bf1339a2
Lorae <stojanovic.lorae@gmail.com>
2024-09-09 14:06:43 -0400

    feat: create script producing multidimensional demographic matrices
    Populated for now with pseudocode and a header - I'm still deciding on the best type of R object for holding this type of data, but am leaning toward a multidimensional array.

---
+++ main.R
@@ -0,0 +1,28 @@
+# main.R
+#
+# Process 2000 IPUMS data into two multidimensional demographic matrices.
+# In both matrices, each each cell represents a unique combination of variables 
+# (age, sex, race, and PUMA).
+# In output matrix `nobs`, the value within each cell represents the number
+# of observations.
+# In output matrix `hhsize`, the value within each cell represents the size of 
+# the household
+#
+# TODO: two matrices not necessary? Each cell has two dimensions of values,
+# nobs and hhsize?
+
+# Pseudocode
+# Create list of attributes and dimensions of each:
+attributes <- list(
+  sex = 
+    c(
+      "M", 
+      "F"
+      ),
+  # I'll do 2 buckets for now for simplicity
+  age = 
+    c(
+      "r00-49",
+      "r5plus"
+    )
+)
