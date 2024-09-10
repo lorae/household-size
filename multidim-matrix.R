f36b6a0644655427c4d187b268ccdcbccd338fd4
Lorae <stojanovic.lorae@gmail.com>
2024-09-10 17:18:44 -0400

    feat: apply improved `generate_bucket_column()` logic to synthetic data `household_data`

--- multidim-matrix.R
+++ multidim-matrix.R
@@ -5,6 +5,7 @@
 
 # ----- Step 0: Source helper functions
 source("src/utils/create-synthetic-data.R")
+source("src/utils/create-categorical-buckets.R")
 
 # Set pseudorandom seed for replicability
 set.seed(42)
@@ -19,17 +20,31 @@ print(household_data)
 # Define a lookup table for ages
 age_lookup_table <- data.frame(
   bucket_name = c("r00_49", "r50plus"),
-  min_value = c(0, 50),
-  max_value = c(49, 200)
+  lower_bound = c(0, 50),
+  upper_bound = c(50, 200)
+)
+
+# Define a lookup table for household income
+hhinc_lookup_table <- data.frame(
+  bucket_name = c("LowIncome", "HighIncome"),
+  lower_bound = c(0, 100000),  
+  upper_bound = c(100000, Inf)
 )
 
 # Example usage with the given data frame 'household_data'
-household_data_bucketed <- categorize_column_buckets(
-  data = household_data, 
-  lookup_table = lookup_table, 
-  column_name = "AGE", 
-  new_column_name = "age_bucket"
-)
+household_data_bucketed <- household_data %>%
+  # Bucket the age categories
+  generate_bucket_column(
+    data = ., 
+    lookup_table = age_lookup_table, 
+    column_name = "AGE"
+  ) %>%
+  # Bucket the income categories
+  generate_bucket_column(
+    data = ., 
+    lookup_table = hhinc_lookup_table, 
+    column_name = "HHINCOME"
+  ) 
 
 # Print the first few rows of the updated data frame to see the new column
-print(head(household_data_bucketed))
+View(household_data_bucketed)
