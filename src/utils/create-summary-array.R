299ec177e5d9000d237e888048b3a8a8c70df152
Lorae <stojanovic.lorae@gmail.com>
2024-09-11 10:06:05 -0400

    feat: create module that will contain helper functions on summarizing arrays

---
+++ src/utils/create-summary-array.R
@@ -0,0 +1,5 @@
+# create-summary-array.R
+# 
+# A module containing helper functions that convert tables of data to summary 
+# arrays, as suggested by Galster (2024). 
+# For full citation, please see the project README.
