8f87b5bd1d622f58bfab477156cca517b9401758
Lorae <stojanovic.lorae@gmail.com>
2024-09-11 15:20:13 -0400

    refactor: write lookup tables row-wise, rather than column-wise
    It's easier for a human to populate a lookup table by  row rather than by column, so I've refactored the code to match this structure.

--- helpers/create-lookup-tables.R
+++ helpers/create-lookup-tables.R
@@ -4,38 +4,48 @@
 # various demographic variables (e.g., age, income) used in the main workflow.
 # These lookup tables are saved as CSV files and can be reused or modified 
 # for different bucketing schemes or sensitivity testing.
-# 
+#
 
 # ----- Age lookup tables -----
 
-# age_buckets00.csv
-age_buckets00 <- data.frame(
-  bucket_name = c("r00_49", "r50plus"),
-  lower_bound = c(0, 50),
-  upper_bound = c(50, 200)
-)
+# Define and populate the age lookup table row-wise
+age_buckets00 <- rbind(
+  c("r00_49", 0, 50),
+  c("r50plus", 50, 200)
+) %>%
+  as.data.frame() %>%
+  setNames(c("bucket_name", "lower_bound", "upper_bound")) %>% # Add column names
+  type.convert(as.is = TRUE) # Convert column encoding to character or numeric
 
 write.csv(age_buckets00, "lookup_tables/age/age_buckets00.csv", row.names = FALSE)
 
 # ----- Household income lookup tables -----
 
 # hhincome_buckets00.csv
-hhincome_buckets00 <- data.frame(
-  bucket_name = c("LowIncome", "HighIncome"),
-  lower_bound = c(0, 100000),  
-  upper_bound = c(100000, Inf)
-)
+hhincome_buckets00 <- rbind(
+  c("LowIncome", 0, 100000),
+  c("HighIncome", 100000, Inf)
+) %>%
+  as.data.frame() %>%
+  setNames(c("bucket_name", "lower_bound", "upper_bound")) %>% # Add column names
+  type.convert(as.is = TRUE) # Convert column encoding to character or numeric
 
-write.csv(hhincome_buckets, "lookup_tables/hhincome/hhincome_buckets00.csv", row.names = FALSE)
+write.csv(hhincome_buckets00, "lookup_tables/hhincome/hhincome_buckets00.csv", row.names = FALSE)
 
 # hhincome_buckets01.csv
-hhincome_buckets01 <- data.frame(
-  bucket_name = c(
-    "r000_020k", "r020k_040k", "r040k_060k", "r060k_080k", 
-    "r080k_100k", "r100k_150k", "r150k_200k", "r200k_300k", "r300kplus"
-  ),
-  lower_bound = c(0, 20000, 40000, 60000, 80000, 100000, 150000, 200000, 300000),
-  upper_bound = c(20000, 40000, 60000, 80000, 100000, 150000, 200000, 300000, Inf)
-)
+hhincome_buckets01 <- rbind(
+  c("r000_020k", 0, 20000),
+  c("r020k_040k", 20000, 40000),
+  c("r040k_060k", 40000, 60000),
+  c("r060k_080k", 60000, 80000),
+  c("r080k_100k", 80000, 100000),
+  c("r100k_150k", 100000, 150000),
+  c("r150k_200k", 150000, 200000),
+  c("r200k_300k", 200000, 300000),
+  c("r300kplus", 300000, Inf)
+) %>%
+  as.data.frame() %>%
+  setNames(c("bucket_name", "lower_bound", "upper_bound")) %>% # Add column names
+  type.convert(as.is = TRUE) # Convert column encoding to character or numeric
 
-write.csv(hhincome_buckets, "lookup_tables/hhincome/hhincome_buckets01.csv", row.names = FALSE)
+write.csv(hhincome_buckets01, "lookup_tables/hhincome/hhincome_buckets01.csv", row.names = FALSE)
