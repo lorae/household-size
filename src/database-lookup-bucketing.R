933a0f911f547bf90e19c09ce50ba9794d712bd4
Lorae <stojanovic.lorae@gmail.com>
2024-09-18 10:26:44 -0400

    feat: demonstrate range matching data values in dplyr using lookup table

--- src/database-lookup-bucketing.R
+++ src/database-lookup-bucketing.R
@@ -19,11 +19,32 @@ source("src/utils/create-synthetic-data.R")
 set.seed(42) # Pseudorandom seed for replicability
 synth_ipums <- generate_household_data() # Generate synthetic ipums data
 
-# Connect to DuckDB
 con <- dbConnect(duckdb::duckdb(), ":memory:")
-
-# Write the IPUMS microdata table to the connection
 dbWriteTable(con, "synth_ipums", synth_ipums) 
+
+# ----- Step 3: Create a lookup table and save to DB ----- #
+age_lookup <- tibble::tibble(
+  bucket_name = c("Under 18", "18-34", "35-49", "Over 50"),
+  lower_bound = c(0, 18, 35, 50),
+  upper_bound = c(18, 35, 50, 200)
+)
+
+dbWriteTable(con, "age_lookup", age_lookup, overwrite = TRUE)
+
+# ----- Step 4: Categorize the data ----- #
+
+# https://stackoverflow.com/questions/75629990/lookup-table-in-r-by-matching-ranges
+synth_ipums |>
+  # For every unique row of data, a new row is generated combining it with the lookup table
+  left_join(age_lookup, by = character()) |>
+  # Then only the rows of the lookup table that match the specified data are kept.
+  # Note that if the lookup table has overlapping ranges that both match the data, it will
+  # produce duplicate entries for the same individual.
+  filter(AGE >= lower_bound & AGE < upper_bound) |>
+  select(-lower_bound, -upper_bound) |> # Clean up extra columns
+  rename(AGE_bucket = bucket_name)
+  
+
 
 # Load the table from the connection
 raw_db <- tbl(con, "synth_ipums")
