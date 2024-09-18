91f62081e2b41e44d2c07a8f745feb2d9f6c1f1e
Lorae <stojanovic.lorae@gmail.com>
2024-09-18 13:38:00 -0400

    test(join_columns): add unit test for function with tibbles

---
+++ tests/testthat/test-join-columns-tibble.R
@@ -0,0 +1,42 @@
+library("testthat")
+library("dplyr")
+library("rprojroot")
+
+# Make sure the working directory is correct
+root <- find_root(is_rstudio_project)
+setwd(root)
+
+# Source the function being tested
+source("src/utils/bucketing-tools.R")
+
+test_that("join_columns works with tibbles in R memory", {
+  
+  # Sample data for data1
+  data1_tb <- tibble(
+    id = c(1, 2, 3, 4, 5, 6, 7),
+    AGE_bucket = c("Over 50", "Under 25", "25-50", "Over 50", "Over 50", "25-50", "Over 50")
+  )
+  
+  # Sample data for data2
+  data2_tb <- tibble(
+    id = c(1, 2, 3, 4, 5, 6, 7),
+    AGE_bucket = c(NA, NA, "30 years old", NA, NA, NA, NA)
+  )
+  
+  # Run the function
+  output_tb <- join_columns(
+    data1 = data1_tb,
+    data2 = data2_tb,
+    column = "AGE_bucket",
+    id = "id"
+  )
+  
+  # Expected result
+  expected_tb <- tibble(
+    id = c(1, 2, 3, 4, 5, 6, 7),
+    AGE_bucket = c("Over 50", "Under 25", "30 years old", "Over 50", "Over 50", "25-50", "Over 50")
+  )
+  
+  # Assert that the output matches the expected result
+  expect_equal(output_tb, expected_tb)
+})
