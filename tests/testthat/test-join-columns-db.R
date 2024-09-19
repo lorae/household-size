e3dae1d3c8beb227d893eef826913d4b2fc1a1a2
Lorae <stojanovic.lorae@gmail.com>
2024-09-18 23:29:21 -0400

    test(join_columns): increase rigor of test data set with additional column
    Previous tests were passed even though the join_columns() function was not behaving as expected: this was because I only had two input columns in the test, `id` and `AGE_bucket`. I've now added an additional neutral data column that has to be carried forward, unchanged, in the output for the test to be passed.

--- tests/testthat/test-join-columns-db.R
+++ tests/testthat/test-join-columns-db.R
@@ -15,16 +15,18 @@ test_that("join_columns works with DuckDB data table", {
   # Create DuckDB connection
   con <- DBI::dbConnect(duckdb::duckdb(), dbdir = ":memory:")
   
-  # Create sample data for data1 and load into DuckDB
+  # Create sample data for data1 and load into DuckDB connection
   data1_tb <- tibble(
     id = c(1, 2, 3, 4, 5, 6, 7),
+    INCOME = c(100000, 0, 50000, 40000, 10000, 55000, 2000),
     AGE_bucket = c("Over 50", "Under 25", "25-50", "Over 50", "Over 50", "25-50", "Over 50")
   )
   dbWriteTable(con, "data1", data1_tb)
   
-  # Create sample data for data2 and load into DuckDB
+  # Create sample data for data2 and load into DuckDB connection
   data2_tb <- tibble(
     id = c(1, 2, 3, 4, 5, 6, 7),
+    INCOME = c(100000, 0, 50000, 40000, 10000, 55000, 2000),
     AGE_bucket = c(NA, NA, "30 years old", NA, NA, NA, NA)
   )
   dbWriteTable(con, "data2", data2_tb)
@@ -47,9 +49,10 @@ test_that("join_columns works with DuckDB data table", {
   # Expected result
   expected_tb <- tibble(
     id = c(1, 2, 3, 4, 5, 6, 7),
+    INCOME = c(100000, 0, 50000, 40000, 10000, 55000, 2000),
     AGE_bucket = c("Over 50", "Under 25", "30 years old", "Over 50", "Over 50", "25-50", "Over 50")
   )
-  
+
   # Assert that the output matches the expected result
   expect_equal(output_tb, expected_tb)
   
