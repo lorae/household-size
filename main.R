b2df2d51ba4e73d9a205fd702ccde45cb14002c5
Lorae <stojanovic.lorae@gmail.com>
2024-09-19 13:48:47 -0400

    refactor: un-chain append_bucket_column() for faster computing
    Lazy loading in DuckDB means that results don't materialize until they are computed - this can result in performance bottlenecks from multiple SQL queries being combined. Forcing a computation after each appended data column results in faster computation.

--- main.R
+++ main.R
@@ -52,7 +52,7 @@ print(
     "seconds")
   )
 
-con <- dbConnect(duckdb::duckdb(), "db/ipums.db", overwrite = TRUE)
+con <- dbConnect(duckdb::duckdb(), ":memory:")
 dbWriteTable(con, "ipums", ipums, overwrite = TRUE)
 
 ipums_db <- tbl(con, "ipums") |>
@@ -66,9 +66,8 @@ ipums_db <- tbl(con, "ipums") |>
 
 # ----- Step 3: Bucket the data ---- #
 
-# Try out the function
+# Append AGE_bucketed according to the lookup table
 ipums_bucketed_db <- ipums_db |>
-  # Append AGE_bucketed according to the lookup table
   append_bucket_column(
     con = con,
     filepath = "lookup_tables/age/age_buckets00.csv", 
@@ -76,24 +75,36 @@ ipums_bucketed_db <- ipums_db |>
     input_column = "AGE", 
     id_column = "id"
   ) |>
-  # Append INCOME_bucketed according to the lookup table
+  compute(name = "ipums_age_bucketed", temporary = FALSE)
+print("Ages bucketed successfully.")
+
+# Append HHINCOME_bucketed according to the lookup table
+ipums_bucketed_db <- ipums_bucketed_db |>
   append_bucket_column(
     con = con,
     filepath = "lookup_tables/hhincome/hhincome_buckets00.csv",
     data = _,
     input_column = "HHINCOME",
     id_column = "id"
-  ) 
+  ) |> 
+  compute(name = "ipums_hhincome_bucketed", temporary = FALSE)
+print("Household incomes bucketed successfully.")
 
-# |>
-# # Append HISPAN_bucketed according to the lookup table
-# append_bucket_column(
-#   con = con,
-#   filepath = "lookup_tables/hispan/hispan_buckets00.csv",
-#   data = _,
-#   input_column = "HISPAN",
-#   id_column = "id"
-# )
+# Append HISPAN_bucketed according to the lookup table
+ipums_bucketed_db <- ipums_bucketed_db |>
+  append_bucket_column(
+    con = con,
+    filepath = "lookup_tables/hispan/hispan_buckets00.csv",
+    data = _,
+    input_column = "HISPAN",
+    id_column = "id"
+  ) |> 
+  compute(name = "ipums_hispan_bucketed", temporary = FALSE)
+print("Ethnicity bucketed successfully.")
+
+# Optional: View a subset of the bucketed data
+# Note: will take about 1 minute to load
+ipums_bucketed_db |> head(50) |> collect() |> View()
 
 # ----- Step 4: Clean up ----- #
 
@@ -104,108 +115,108 @@ DBI::dbDisconnect(con)
 ######################################################################
 ##### NEARLY DEPRECATED CODE THAT I'M NOT YET READY TO PART WITH #####
 ######################################################################
-
-# ----- Step 3: Produce aggregate household sizes in data table
-
-# Convert your large data frame to data.table for faster processing
-dt <- as.data.table(data_bucketed)
-
-# Compute weighted mean directly using data.table's grouping and add counts
-# TODO: replace HHWT with the correct person-level weight.
-aggregate_dt <- dt[, .(
-  weighted_mean = weighted.mean(PERNUM, w = HHWT, na.rm = FALSE),  # Weighted mean calculation
-  count = .N  # Count of observations in each group
-), by = .(
-  AGE_bucket, 
-  HHINCOME_bucket, 
-  SEX, 
-  RACE_ETH_bucket
-)
-]
-
-# Print the resulting data table
-print(aggregate_dt)
-
-# Example lookup tables with desired order
-sex_levels <- c(1, 2)
-
-# Convert to factor with specified levels
-aggregate_dt[, AGE_bucket]
-aggregate_dt[, HHINCOME_bucket]
-aggregate_dt[, RACE_ETH_bucket]
-aggregate_dt[, SEX := factor(SEX, levels = sex_levels)]
-
-# Sort by age, income, race/ethnicity, and sex
-setorder(aggregate_dt, AGE_bucket, HHINCOME_bucket, RACE_ETH_bucket, SEX)
-
-# View the sorted data
-print(aggregate_dt)
-
-# Export the data.table to a CSV file
-fwrite(aggregate_dt, "output/aggregate_dt_sorted.csv")
-
-
-# ----- Step EXTRA: Explore the data
-
-# Learn higher level info about variables
-View(ddi$var_info)
-
-# Learn what the variables are!
-names(data) # not very informative
-
-# Variable labels provide additional insight into what a cryptically-labelled
-# column represents
-ipums_var_label(ddi, PERNUM)
-
-# Value labels decode numeric codes into their descriptions
-ipums_val_labels(ddi, SEX)
-
-# A nice static HTML page labelling the data. This can be useful for understanding
-# a dataset after importing it.
-ipums_view(ddi)
-
-# Check for missing year
-if (any(is.na(data$YEAR))) {
-  warning("There are missing values in the YEAR column.")
-}
-
-# Data from 2000
-data2000 <- data |>
-  filter(YEAR == 2000)
-
-# Data from 2020
-data2020 <- data |>
-  filter(YEAR == 2020)
-
-# Number of observations
-nrow(data2000)
-nrow(data2020)
-
-## Number of households. There are two ways to count the number of households.
-
-# Method 1: Count the number of "person number 1" (within each n-person household, persons 
-# are numbered from 1 to n using the PERNUM variable).
-data2000 |> 
-  filter(PERNUM == 1) |>
-  nrow()
-
-# Method 2: Count the number of unique serial numbers
-length(unique(data2000$SERIAL))
-
-## Average household size
-
-# In 2000
-data2000 |>
-  filter(PERNUM == 1) |>
-  weighted.mean(x = .$NUMPREC, w = .$HHWT)
-
-# In 2020
-data2020 |>
-  filter(PERNUM == 1) |>
-  weighted.mean(x = .$NUMPREC, w = .$HHWT)
-
-
-
-
-
-
+# 
+# # ----- Step 3: Produce aggregate household sizes in data table
+# 
+# # Convert your large data frame to data.table for faster processing
+# dt <- as.data.table(data_bucketed)
+# 
+# # Compute weighted mean directly using data.table's grouping and add counts
+# # TODO: replace HHWT with the correct person-level weight.
+# aggregate_dt <- dt[, .(
+#   weighted_mean = weighted.mean(PERNUM, w = HHWT, na.rm = FALSE),  # Weighted mean calculation
+#   count = .N  # Count of observations in each group
+# ), by = .(
+#   AGE_bucket, 
+#   HHINCOME_bucket, 
+#   SEX, 
+#   RACE_ETH_bucket
+# )
+# ]
+# 
+# # Print the resulting data table
+# print(aggregate_dt)
+# 
+# # Example lookup tables with desired order
+# sex_levels <- c(1, 2)
+# 
+# # Convert to factor with specified levels
+# aggregate_dt[, AGE_bucket]
+# aggregate_dt[, HHINCOME_bucket]
+# aggregate_dt[, RACE_ETH_bucket]
+# aggregate_dt[, SEX := factor(SEX, levels = sex_levels)]
+# 
+# # Sort by age, income, race/ethnicity, and sex
+# setorder(aggregate_dt, AGE_bucket, HHINCOME_bucket, RACE_ETH_bucket, SEX)
+# 
+# # View the sorted data
+# print(aggregate_dt)
+# 
+# # Export the data.table to a CSV file
+# fwrite(aggregate_dt, "output/aggregate_dt_sorted.csv")
+# 
+# 
+# # ----- Step EXTRA: Explore the data
+# 
+# # Learn higher level info about variables
+# View(ddi$var_info)
+# 
+# # Learn what the variables are!
+# names(data) # not very informative
+# 
+# # Variable labels provide additional insight into what a cryptically-labelled
+# # column represents
+# ipums_var_label(ddi, PERNUM)
+# 
+# # Value labels decode numeric codes into their descriptions
+# ipums_val_labels(ddi, SEX)
+# 
+# # A nice static HTML page labelling the data. This can be useful for understanding
+# # a dataset after importing it.
+# ipums_view(ddi)
+# 
+# # Check for missing year
+# if (any(is.na(data$YEAR))) {
+#   warning("There are missing values in the YEAR column.")
+# }
+# 
+# # Data from 2000
+# data2000 <- data |>
+#   filter(YEAR == 2000)
+# 
+# # Data from 2020
+# data2020 <- data |>
+#   filter(YEAR == 2020)
+# 
+# # Number of observations
+# nrow(data2000)
+# nrow(data2020)
+# 
+# ## Number of households. There are two ways to count the number of households.
+# 
+# # Method 1: Count the number of "person number 1" (within each n-person household, persons 
+# # are numbered from 1 to n using the PERNUM variable).
+# data2000 |> 
+#   filter(PERNUM == 1) |>
+#   nrow()
+# 
+# # Method 2: Count the number of unique serial numbers
+# length(unique(data2000$SERIAL))
+# 
+# ## Average household size
+# 
+# # In 2000
+# data2000 |>
+#   filter(PERNUM == 1) |>
+#   weighted.mean(x = .$NUMPREC, w = .$HHWT)
+# 
+# # In 2020
+# data2020 |>
+#   filter(PERNUM == 1) |>
+#   weighted.mean(x = .$NUMPREC, w = .$HHWT)
+# 
+# 
+# 
+# 
+# 
+# 
