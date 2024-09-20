18008a5c9d31d26dcefee43d6b26cc7c80958e33
Lorae <stojanovic.lorae@gmail.com>
2024-09-20 11:10:23 -0400

    refactor(weighted_mean): remove compute() from within function and replace magrittr pipes for base r pipes
    I find it simpler write lazy functions, only forcing computation within the pipeline itself, if needed. Doing so comports more closely with convention in the rest of the code.

--- src/utils/aggregation-tools.R
+++ src/utils/aggregation-tools.R
@@ -8,14 +8,13 @@ weighted_mean <- function(data, value_column, weight_column, group_by_columns) {
   
   # Calculate the weighted mean, sum of weights, and count of observations
   data %>%
-    group_by(!!!group_by_cols) %>%
+    group_by(!!!group_by_cols) |>
     summarize(
       total_value_weighted = sum(!!value_col * !!weight_col, na.rm = TRUE),
       total_weight = sum(!!weight_col, na.rm = TRUE),
       sum_weights = sum(!!weight_col, na.rm = TRUE),  # Sum of weights
       count = n()
-    ) %>%
-    mutate(weighted_mean = total_value_weighted / total_weight) %>%
-    select(!!!group_by_cols, count, sum_weights, weighted_mean) %>%
-    compute(name = "weighted_mean", temporary = FALSE)
+    ) |>
+    mutate(weighted_mean = total_value_weighted / total_weight) |>
+    select(!!!group_by_cols, count, sum_weights, weighted_mean)
 }
