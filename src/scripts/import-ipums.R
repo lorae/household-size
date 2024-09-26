ab9f73baf174d8c8a13b38d20af8b340526d0a4c
Lorae <stojanovic.lorae@gmail.com>
2024-09-26 13:37:01 -0400

    feat: create self-contained raw ipums import script
    This script deprecates portions of main.R that import data and write to a database connection.

---
+++ src/scripts/import-ipums.R
@@ -0,0 +1,33 @@
+# import-ipums.R
+#
+# This script processes raw IPUMS data and saves it in a DuckDB file.
+# 
+# Current data: usa_00004 (2000, 2020). This file is .gitignored.
+# TODO: replace manual data pull with an API call for reproducibility.
+#
+# For more on IPUMS and ipumsr: https://www.youtube.com/watch?v=OT6upQ1dBgU
+#
+# According to the Census Bureau: "A combination of SAMPLE and SERIAL provides a unique 
+# identifier for every household in the IPUMS; the combination of SAMPLE, SERIAL, 
+# and PERNUM uniquely identifies every person in the database."
+
+# ----- Step 0: Load packages ----- #
+library("dplyr")
+library("duckdb")
+
+# ----- Step 1: Load and process IPUMS data ----- #
+
+ddi <- read_ipums_ddi("usa_00004.xml")
+ipums_tb <- read_ipums_micro(ddi, var_attrs = c()) 
+
+# Create unique person ID and reorder columns
+ipums_tb_id <- ipums_tb |> 
+  mutate(id = paste(SAMPLE, SERIAL, PERNUM, sep = "_")) |> 
+  select(id, everything())
+
+# ----- Step 2: Save to DuckDB ----- #
+
+con <- dbConnect(duckdb::duckdb(), "db/ipums.duckdb")
+dbWriteTable(con, "ipums", ipums_tb_id, overwrite = TRUE)
+DBI::dbDisconnect(con)
+
