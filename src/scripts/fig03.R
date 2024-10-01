5f45c5588a0ed539207360a04bc27ddf9550a50a
Lorae <stojanovic.lorae@gmail.com>
2024-10-01 18:36:14 -0400

    refactor: functionalize map generation

--- src/scripts/fig03.R
+++ src/scripts/fig03.R
@@ -4,7 +4,6 @@
 # 2000 and 2020, by State
 
 # ----- Step 0: Load required packages ----- #
-library("magrittr")
 library("dplyr")
 library("duckdb")
 library("ipumsr")
@@ -14,23 +13,41 @@ library("readr")
 library("purrr")
 library("sf")
 library("ggplot2")
-library("writexl")
 library("tigris")
+library("rlang")
 library("DBI")
 
-# ----- Step 1: Source helper functions ----- #
-
-source("src/utils/bucketing-tools.R")
+# ----- Step 1: Source and define helper functions ----- #
 source("src/utils/aggregation-tools.R")
-source("src/utils/data-validation.R")
 
+# Creates a 2x2 matrix representing the rotation transformation relative to angle
+# a
+rot <- function(a) {
+  matrix(
+    c(cos(a), sin(a), -sin(a), cos(a)), 
+    2, 2
+    )
+}
 
-# ----- Step 2: Load in data ----- #
+# Moves a state according to custom specifications
+transform_state <- function(
+    df, 
+    state_fp, 
+    rotation_angle, 
+    scale_factor, 
+    shift_coords
+    ) {
+  state <- df %>% filter(STATEFIP == state_fp)
+  state_geom <- st_geometry(state)
+  state_centroid <- st_centroid(st_union(state_geom))
+  rotated_geom <- (state_geom - state_centroid) * rot(rotation_angle * pi / 180) / scale_factor + state_centroid + shift_coords
+  state %>% st_set_geometry(rotated_geom) %>% st_set_crs(st_crs(df))
+}
 
+# ----- Step 3: Load in data ----- #
 con <- dbConnect(duckdb::duckdb(), "data/db/ipums-processed.duckdb")
 
-# ----- Step 3: Calculate means by state and year
-
+# Calculate means by state and year
 state_mean_db <- weighted_mean(
   data = tbl(con, "ipums_bucketed"),
   value_column = "NUMPREC",
@@ -38,90 +55,63 @@ state_mean_db <- weighted_mean(
   group_by_columns = c("STATEFIP", "YEAR")
 )
 
-# Subtract 2000 weighted mean household size from 2020 weighted mean household size,
-# and save to `state_mean_diff_tb`
+# Calculate differences in weighted means between 2000 and 2020
 state_mean_diff_tb <- difference_means(
-    data2000 = state_mean_db |> filter(YEAR == 2000), 
-    data2020 = state_mean_db |> filter(YEAR == 2020),
-    match_by = "STATEFIP",
-    diff_by = "weighted_mean",
-    keep = c("count", "sum_weights") # Columns to keep in addition to the diff_by column
-) |>
-  collect()
-  
-  
+  data2000 = state_mean_db |> filter(YEAR == 2000), 
+  data2020 = state_mean_db |> filter(YEAR == 2020),
+  match_by = "STATEFIP",
+  diff_by = "weighted_mean",
+  keep = c("count", "sum_weights")
+) |> collect()
 
-df <- tigris::states(cb = T, class = 'sf')
+# ----- Step 4: Load and process state shapefile ----- #
 
-df <- df %>% filter(!STATEFP %in% c('60', '66', '69', '72', '78'))
+# Load shapefiles from tigris
+state_sf <- tigris::states(cb = TRUE, class = 'sf') |>
+  rename(STATEFIP = STATEFP) |> # For consistency with household size data
+  filter(!STATEFIP %in% c('60', '66', '69', '72', '78')) |># Remove excluded states, like Puerto Rico
+  st_transform(crs = "+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs")
 
-crs_lambert <- "+proj=laea +lat_0=45 +lon_0=-100 +x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs"
+# Rotate and move Alaska and Hawaii to fit on map
+alaska <- transform_state(state_sf, "02", -39, 2.3, c(1000000, -5000000))
+hawaii <- transform_state(state_sf, "15", -35, 1, c(5200000, -1400000))
 
-df <- df %>%
-  st_transform(crs = crs_lambert)
+# Final map after transforming non-contiguous states
+state_sf_final <- state_sf %>%
+  filter(!STATEFIP %in% c("02", "15")) %>%
+  bind_rows(alaska, hawaii)
 
+# Join the state data with household size differences
+state_sf_final_diff <- state_sf_final %>%
+  left_join(state_mean_diff_tb, by = "STATEFIP")
 
-alaska <- df %>% filter(STATEFP == "02")
-alaska_g <- st_geometry(alaska)
-alaska_centroid <- st_centroid(st_union(alaska_g))
+# ----- Step 6: Generate Figures ----- #
 
-rot <- function(a) matrix(c(cos(a), sin(a), -sin(a), cos(a)), 2, 2)
-
-alaska_trans <- (alaska_g - alaska_centroid) * rot(-39 * pi/180) / 2.3 + alaska_centroid + c(1000000, -5000000)
-
-alaska <- alaska %>% st_set_geometry(alaska_trans) %>% st_set_crs(st_crs(df))
-
-
-hawaii <- df %>% filter(STATEFP == "15")
-
-hawaii_g <- st_geometry(hawaii)
-hawaii_centroid <- st_centroid(st_union(hawaii_g))
-
-hawaii_trans <- (hawaii_g - hawaii_centroid) * rot(-35 * pi/180) + hawaii_centroid + c(5200000, -1400000)
-hawaii <- hawaii %>% st_set_geometry(hawaii_trans) %>% st_set_crs(st_crs(df))
-
-tnc_map_final <- df %>%
-  filter(!STATEFP %in% c("02", "15")) %>%
-  rbind(alaska) %>%
-  rbind(hawaii)
-
-tnc_map_final_diff <- tnc_map_final %>%
-  left_join(state_mean_diff_tb, by = c("STATEFP" = "STATEFIP"))
-
-# Adjust the choropleth map with additional break
-fig03 <- ggplot(tnc_map_final_diff) + 
+# Choropleth map (color version)
+fig03 <- ggplot(state_sf_final_diff) + 
   geom_sf(aes(geometry = geometry, fill = diff), color = "black", size = 0.5) +
   scale_fill_gradient2(
     name = "2000 to 2020 \ndifference",
-    low = "#577590",
-    mid = "white",
-    high = "#F94144",
-    midpoint = 0,
-    breaks = seq(from =-0.2, to = 0.05, by = 0.05) # Add the desired breaks, including a positive number
+    low = "#577590", mid = "white", high = "#F94144", midpoint = 0,
+    breaks = seq(from = -0.2, to = 0.05, by = 0.05)
   ) +
   theme_void()
 
 # Save the plot as a PNG file
 ggsave("results/fig03.png", plot = fig03, width = 6.5, height = 5, dpi = 300)
 
-
-##### Alternate
-# color scale is Black and White
-
-# Create the black and white version of the choropleth map with gradual gray shades
-fig03a <- ggplot(tnc_map_final_diff) + 
+# Choropleth map (black and white version)
+fig03a <- ggplot(state_sf_final_diff) + 
   geom_sf(aes(geometry = geometry, fill = diff), color = "black", size = 0.5) +
   scale_fill_gradient(
     name = "2000 to 2020 \ndifference",
-    low = "black",
-    high = "white",
-    limits = c(-0.2, 0.05),  # Set the limits to control the range
-    oob = scales::squish  # Squish values outside the range into the limit
+    low = "black", high = "white", limits = c(-0.2, 0.05),
+    oob = scales::squish
   ) +
   theme_void()
 
 # Save the black and white plot as fig03a
 ggsave("results/fig03a.png", plot = fig03a, width = 6.5, height = 5, dpi = 300)
 
-# Disconnect from DuckDB
-DBI::dbDisconnect(con)
+# ----- Step 7: Clean up ----- #
+dbDisconnect(con)
