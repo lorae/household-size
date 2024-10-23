640e9a15ff56d98f2658ca0889d63692df85061e
Lorae <10428186+lorae@users.noreply.github.com>
2024-10-23 10:21:43 -0400

    feat: several changes I forgot to commit (whoops)
    Not my finest commit - but picking up this project after a few weeks' hiatus, and I don't know why I didn't commit these. So here you go.

--- src/scripts/import-ipums.R
+++ src/scripts/import-ipums.R
@@ -14,7 +14,7 @@ library("ipumsr")
 
 # ----- Step 1: Load and process IPUMS data ----- #
 
-ddi <- read_ipums_ddi("data/ipums-microdata/usa_00004.xml")
+ddi <- read_ipums_ddi("data/ipums-microdata/usa_00010.xml")
 ipums_tb <- read_ipums_micro(ddi, var_attrs = c()) 
 
 # ----- Step 2: Save to DuckDB ----- #
@@ -23,3 +23,27 @@ con <- dbConnect(duckdb::duckdb(), "data/db/ipums-raw.duckdb")
 dbWriteTable(con, "ipums", ipums_tb, overwrite = TRUE)
 DBI::dbDisconnect(con)
 
+# ----- Step 3: Save helpful reference documentation ----- #
+
+# Interactive HTML document outlining the variables from the 
+# data pull
+ipums_view(ddi, out_file = "docs/ipums-data-dictionary.html", launch = FALSE)
+
+# A list which provides the value labels for every variable
+# in the pull. The tibble of value labels for a variable
+# VARNAME, for example, can be accessed through  the following
+# code:
+# `my_tibble <- value_labels_list$VARNAME`
+
+value_labels_list <- lapply(seq_len(nrow(ddi$var_info)), function(i) {
+  val_labels <- ddi$var_info$val_labels[[i]]
+  if (nrow(val_labels) > 0) {
+    return(val_labels)
+  } else {
+    return(NULL)
+  }
+})
+
+names(value_labels_list) <- ddi$var_info$var_name
+
+save(value_labels_list, file = "docs/ipums_value_labels.RData")
