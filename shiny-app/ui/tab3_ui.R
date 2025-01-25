f264d47f3c066d23c103cd949e14790f8db00afa
Lorae <10428186+lorae@users.noreply.github.com>
2025-01-25 15:31:17 -0500

    feat(app): introduce counterfactual results in 'New Results' tab

--- shiny-app/ui/tab3_ui.R
+++ shiny-app/ui/tab3_ui.R
@@ -1,5 +1,13 @@
 # Define the UI
 tab3_ui <- fluidPage(
   tags$h3("Results", id = "results"),
-  p("")
+  p("Figure 3.1 shows counterfactual household sizes under a variety of controls.
+    A 1 indicates that a control was used in a given counterfactual simulation, and
+    a 0 indicates that the control was not used."),
+  
+  DTOutput("fig3.1"),
+  
+  p("Figure 3.2 models occupancy per bedroom under an identical set of counterfactuals."),
+
+  DTOutput("fig3.2"),
 )
