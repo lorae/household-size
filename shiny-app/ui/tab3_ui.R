4db84adc1dbc10ac5ca44e6376526794d55c6b9e
Lorae <10428186+lorae@users.noreply.github.com>
2025-02-05 09:32:01 -0500

    docs(app): clarify numbering in tab 3 and improve text description

--- shiny-app/ui/tab3_ui.R
+++ shiny-app/ui/tab3_ui.R
@@ -10,15 +10,15 @@ tab3_ui <- fluidPage(
   "))),
   
   tags$h3("Counterfactuals", id = "results"),
-  p("Figure 3.1 shows counterfactual household sizes under a variety of controls.
+  p("Table 3.1 shows counterfactual household sizes under a variety of controls.
     A 1 indicates that a control was used in a given counterfactual simulation, and
     a 0 indicates that the control was not used."),
   
-  DTOutput("fig3.1"),
+  DTOutput("tab3.1"),
   
-  p("Figure 3.2 models occupancy per bedroom under an identical set of counterfactuals."),
+  p("Table 3.2 models occupancy per bedroom under an identical set of counterfactuals."),
 
-  DTOutput("fig3.2"),
+  DTOutput("tab3.2"),
   
   tags$h3("State-level Dot Plots", id = "boxwhisker"),
   
@@ -28,8 +28,8 @@ tab3_ui <- fluidPage(
     As such, some less populated statess have only one CPUMA0010 region, while others
     have many."),
   
-  p("Tables 3.1 and 3.2 display CPUMA-level actual versus counterfactual results. 
-    The black vertical line at 0 represents a situation where a state's average number
+  p("Tables 3.3 and 3.4 display CPUMA-level actual versus counterfactual results. 
+    The black vertical line at 0 represents a situation where a regions's average number
     of persons per household or persons per bedroom in 2019 is exactly equal to the counterfactual
     based in 2000. A value to the right of the solid black line indicates that the
     region's measured density in 2019 exceeds the counterfactual expectations. A value
@@ -37,11 +37,11 @@ tab3_ui <- fluidPage(
     counterfactual expectations. Observations are at the CPUMA0010 level."),
   
   p("The dashed red and blue lines correspond to the weighted mean and weighted median
-    total for the given state. Values are measured in persons per bedroom or persons
-    per household. Weights are given by CPUMA0010-level population in 2019.
-    So, for example, in figure 3.1, the weighted median of 0.049 for 
-    Alabama indicates that on average, each Alabama household had 0.049 more people than 
-    expected based on the counterfactual."),
+    total for the given state. Values are measured in persons
+    per household (table 3.3) or persons per bedroom (table 3.4). Weights are given by CPUMA0010-level population in 2019.
+    So, for example, in table 3.3, the weighted median of 0.049 for 
+    Alabama indicates that on average, Alabama households had 0.049 more people than 
+    expected from the counterfactual."),
   
   p("[TODO: add persons per household for each state as a column in the chart.]"),
   
@@ -54,7 +54,12 @@ tab3_ui <- fluidPage(
   
   tags$h4("Household size counterfactuals, by state"),
   
-  p(strong("Table 3.1: Household size counterfactuals, by state and CPUMA0010")),
+  p(strong("Table 3.3: Household size counterfactuals, by state and CPUMA0010")),
+  
+  p("The dot plots are cut off at +0.5 and -0.5. A very limited number of observations (roughly
+    3 total) [TODO: get exact amount. Mark these dots as red on the dotplot] of >1000 
+    CPUMA0010s [TODO: get exact number] are not displayed because they fall outside the (-0.5, 
+    +0.5) range"),
 
-  reactableOutput("tab3.1")
+  reactableOutput("tab3.3")
 )
