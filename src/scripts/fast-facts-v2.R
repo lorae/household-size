1d9deaff4f4c05d436b5567d6c61601c45507eab
Lorae T. Stojanovic <ls4540@della8.princeton.edu>
2025-02-11 18:59:06 -0500

    feat(fig01): save results and organize fast-facts-v2 better

--- src/scripts/fast-facts-v2.R
+++ src/scripts/fast-facts-v2.R
@@ -131,14 +131,15 @@ tabulate_summary_2year(data = ipums_db, years = c(2000,2019), group_by = "SEX", 
 tabulate_summary_2year(data = ipums_db, years = c(2000,2019), group_by = c())
 
 
-# ----- Step 4a: Fast facts! ----- #
-# Household size in 2000 and 2019
+# ----- Step 4a: RESULTS - Household size in 2000 and 2019 ----- #
+# TODO: export as table to Shiny app
 tabulate_summary_2year(data = ipums_db, years = c(2000,2019), group_by = c())
-  
-# ----- Step 4b: Fast facts! ----- #
-# Household size in 2000 and 2019 by race/ethnicity
+
+# ----- Step 4b: RESULTS - Household size in 2000 and 2019 by race/ethnicity ----- #
+# TODO: export as table to Shiny app
 tabulate_summary_2year(data = ipums_db, years = c(2000,2019), group_by = "RACE_ETH_bucket")
 
+### # FIGURE 1: Create the bar plot with side-by-side bars for 2000 and 2019
 # Create data for bar plot
 race_summary_2000 <- tabulate_summary(data = ipums_db, year = 2000, group_by = "RACE_ETH_bucket") |> mutate(year = 2000)
 race_summary_2019 <- tabulate_summary(data = ipums_db, year = 2019, group_by = "RACE_ETH_bucket") |> mutate(year = 2019)
@@ -147,7 +148,7 @@ race_summary <- union_all(race_summary_2000, race_summary_2019) # Row bind the t
 # Define a single main color for all bars
 main_color <- "steelblue"
 
-# FIGURE 1: Create the bar plot with side-by-side bars for 2000 and 2019
+# Generate the plot
 fig01 <- ggplot(race_summary, aes(x = subgroup, y = hhsize, fill = factor(year))) +
   geom_bar(stat = "identity", position = position_dodge(width = 0.8), 
            width = 0.8, color = "black") +  # Bar border
@@ -157,7 +158,7 @@ fig01 <- ggplot(race_summary, aes(x = subgroup, y = hhsize, fill = factor(year))
   scale_fill_manual(
     values = c("2000" = alpha(main_color, 0.4), "2019" = alpha(main_color, 0.8)), 
     name = "") +  # Ensures the legend colors match bar colors
-  labs(y = "People per household") +
+  labs(y = "Average Household Size") +
   theme_minimal() +
   theme(
     axis.text.x = element_text(angle = 0, hjust = 0.5),
@@ -167,12 +168,18 @@ fig01 <- ggplot(race_summary, aes(x = subgroup, y = hhsize, fill = factor(year))
     plot.margin = margin(t = 10, r = 10, b = 0, l = 10)
   )
 
+# Save the plot
+ggsave("results/fig01.png", plot = fig01, width = 6.5, height = 3.5, dpi = 500)
 
-# Household size in 2000 and 2019 by age
+# ----- Step 4c: RESULTS - Household size in 2000 and 2019 by age ----- #
+# TODO: export as table to Shiny app
 age_summary <- tabulate_summary_2year(data = ipums_db, years = c(2000,2019), group_by = "AGE")
+
 # Ages with the largest household sizes
+# TODO: highlight these rows in the Shiny app table
 age_summary |> slice_max(hhsize_2000, n = 1)
 age_summary |> slice_max(hhsize_2019, n = 1)
+
 # Graph
 # Filter data to only include ages up to 90
 age_summary_filtered <- age_summary |>
