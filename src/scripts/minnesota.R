47fb6a785c15e6772eab9b326ebc69c46767888c
Lorae T. Stojanovic <ls4540@della-vis1.princeton.edu>
2025-03-27 10:35:47 -0400

    feat: create outline for minnesota.R script

--- src/scripts/minnesota.R
+++ src/scripts/minnesota.R
@@ -1,3 +1,14 @@
 # minnesota.R
 # Specific facts and figures helpful for Minnesota
 
+# ----- Step 0: Load necessary modules, libraries ----- #
+
+# ----- Step 1: Load database and add needed columns ----- #
+
+# ----- Step 2: Calculate counterfactual ----- #
+
+# ----- Step 3: Generate counterfactual by CPUMA map ----- #
+
+# ----- Step 4: Generate bar graph showing actual, counterfactual, diff (fig 3) ----- #
+
+# ----- Step 5: Calculate housing supply shortage / surfeit ----- #
