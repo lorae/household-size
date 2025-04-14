c39b95b0a62edc398d2168ae398762255ed7d8b3
lorae <stojanovic.lorae@gmail.com>
2025-04-14 17:56:21 -0400

    refactor(hist): completely overhaul to manually create frequency plot Rather than using the geom_hist function. Allows for more customization and better flexibility of labelling, particularly when limiting to xmax. Also, will let me easily print frequencies above the bars.

--- src/utils/graphing-tools.R
+++ src/utils/graphing-tools.R
@@ -1,6 +1,7 @@
 hist <- function(
     data,
-    colname = "NUMPREC",
+    data_col = "NUMPREC",
+    weight_col = "PERWT",
     title = "",
     xtitle = "Number of people in HH", # X-axis title
     ytitle = "Proportion", # Y-axis title
@@ -8,40 +9,50 @@ hist <- function(
     ymax = NA, # If specified, max y-value on chart
     bar_fill = list(color = "skyblue", alpha = 0.5)
 ) {
-  raw_data <- data |> pull({{ colname }}) 
+  # Produce a frequency table with the first column as the `data_col` and the
+  # second column as the sum of the weights in `weight_col`
+  freq_raw <- data |> 
+    select(all_of(c(data_col, weight_col))) |>
+    group_by(.data[[data_col]]) |>
+    summarise(freq = sum(.data[[weight_col]], na.rm = TRUE), .groups = "drop") |>
+    arrange(.data[[data_col]]) |>
+    collect()
   
-  # Cap values at xmax
-  if (!is.na(xmax)) {
-    raw_data <- pmin(raw_data, xmax)
-    x_breaks <- seq(from = 1, to = xmax, by = 1)
-    x_labels <- as.character(x_breaks)
-    x_labels[length(x_labels)] <- paste0(xmax, "+")
-  } else {
-    xmax = max(raw_data)
-    x_breaks <- seq(from = 1, to = xmax, by = 1)
-    x_labels <- as.character(x_breaks)
-  }
+  # Maximum data_col value observed in the data
+  xmax_observed <- freq_raw[[data_col]] |> max()
   
-  # Warning: ymax
-  # TODO
+  # Fill in the frequency table with all whole numbers spanning from 1 to the 
+  # maximum observed value of `data_col` (i.e. `xmax_observed). Fill in these extra 
+  # entries as 0
+  filler_values <- tibble(!!data_col := 1:xmax_observed)
+  freq_filled <- filler_values |>
+    left_join(freq_raw, by = data_col) |>
+    mutate(freq = replace_na(freq, 0)) # fill remaining NAs with 0
+
+  freq_filled_label <- freq_filled |>
+    mutate(
+      # If xmax is specified, assign `label` to read as "`xmax`+"
+      # Else, assign the label to just be the character version of `data_col`
+      label = case_when(
+        !is.na(xmax) & .data[[data_col]] >= xmax ~ paste0(xmax, "+"),
+        TRUE ~ as.character(.data[[data_col]])
+      )
+    ) |>
+    # Now sum all values with the same `label` (i.e. all those `xmax`+ entries)
+    group_by(label) |>
+    summarize(freq = sum(freq), .groups = "drop") |>
+    mutate(proportion = freq / sum(freq))
   
-  plot_df <- data.frame(raw_data)
-  
-  # Base plot
-  p <- 
-    ggplot(
-      plot_df, 
-      aes(x = raw_data, y = after_stat(density))) +
-      geom_histogram(
-        binwidth = 1, 
-        color = "black", 
-        fill = bar_fill$color,
-        alpha = bar_fill$alpha
-        ) +
-    scale_x_continuous(
-      breaks = x_breaks,
-      labels = x_labels
-      ) +
+  #print(freq_filled_label)
+
+  # Plot it!
+  p <- ggplot(freq_filled_label, aes(x = label, y = proportion)) +
+    geom_col(
+      width = 1,
+      fill = bar_fill$color,
+      alpha = bar_fill$alpha,
+      color = "black"
+    ) +
     scale_y_continuous(limits = if (!is.na(ymax)) c(0, ymax) else NULL) +
     labs(
       title = title,
