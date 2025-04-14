ee95090e1cd1fc29698b4fc479b1fd97bc8f8a88
lorae <stojanovic.lorae@gmail.com>
2025-04-14 16:26:51 -0400

    feat(hist): enforce bin label of 1, even when no observations of 1 are present

--- src/utils/graphing-tools.R
+++ src/utils/graphing-tools.R
@@ -5,19 +5,20 @@ hist <- function(
     xtitle = "Number of people in HH", # X-axis title
     ytitle = "Proportion", # Y-axis title
     xmax = NA,
-    ymax = NA # If specified, max y-value on chart
+    ymax = NA, # If specified, max y-value on chart
+    bar_fill = list(color = "skyblue", alpha = 0.5)
 ) {
   raw_data <- data |> pull({{ colname }}) 
   
   # Cap values at xmax
   if (!is.na(xmax)) {
     raw_data <- pmin(raw_data, xmax)
-    x_breaks <- seq(from = min(raw_data), to = xmax, by = 1)
+    x_breaks <- seq(from = 1, to = xmax, by = 1)
     x_labels <- as.character(x_breaks)
     x_labels[length(x_labels)] <- paste0(xmax, "+")
   } else {
     xmax = max(raw_data)
-    x_breaks <- seq(from = min(raw_data), to = xmax, by = 1)
+    x_breaks <- seq(from = 1, to = xmax, by = 1)
     x_labels <- as.character(x_breaks)
   }
   
@@ -31,10 +32,16 @@ hist <- function(
     ggplot(
       plot_df, 
       aes(x = raw_data, y = after_stat(density))) +
-      geom_histogram(binwidth = 1, color = "black", fill = "skyblue") +
+      geom_histogram(
+        binwidth = 1, 
+        color = "black", 
+        fill = bar_fill$color,
+        alpha = bar_fill$alpha
+        ) +
     scale_x_continuous(
       breaks = x_breaks,
-      labels = x_labels
+      labels = x_labels,
+      limits = c(1, xmax)
       ) +
     scale_y_continuous(limits = if (!is.na(ymax)) c(0, ymax) else NULL) +
     labs(
