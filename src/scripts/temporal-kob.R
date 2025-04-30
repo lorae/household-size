3cd4ead555517d0a722ae57e9f41fe13aebd1c19
lorae <stojanovic.lorae@gmail.com>
2025-04-30 14:44:01 -0400

    feat: accurately calculate pop proportions

--- src/scripts/temporal-kob.R
+++ src/scripts/temporal-kob.R
@@ -67,8 +67,11 @@ known_varnames <- c(
   "EDUC_bucket", "INCTOT_cpiu_2010_bucket", "tenure"
 )
 
-# Step 1: Exclude intercept and extract varname + value
-coef_df <- coef_df |>
+intercept_row <- coef_df |>
+  filter(name == "(Intercept)") |>
+  mutate(varname = NA_character_, value = NA_character_)
+
+non_intercepts <- coef_df |>
   filter(name != "(Intercept)") |>
   mutate(
     varname = map_chr(name, function(nm) {
@@ -79,16 +82,52 @@ coef_df <- coef_df |>
     value = str_remove(name, varname)
   )
 
+coef_df <- bind_rows(intercept_row, non_intercepts)
+
+
+
 get_weighted_count <- function(varname, value, year) {
+  # Return NA for intercept or missing input
+  if (is.na(varname) || is.na(value)) {
+    return(NA_real_)
+  }
+  
+  # Coerce specific label strings to their underlying codes
+  if (value == "male" && varname == "sex") {
+    value <- 1
+    varname <- "SEX"
+  } else if (value == "homeowner" && varname == "tenure") {
+    value <- 1
+    varname <- "OWNERSHP"
+  }
+  
   ipums_db |>
     filter(YEAR == !!year, GQ %in% c(0, 1, 2)) |>
-    filter(!!sym(varname) == value) |>
+    filter(!!sym(varname) == !!value) |>
     summarise(weighted_count = sum(PERWT), na.rm = TRUE) |>
     collect() |>
     pull(weighted_count)
 }
 
-coef_df2 <- coef_df |>
+
+coef <- coef_df |>
   mutate(
-    weighted_count_2000 = map2_dbl(varname, value, ~ get_weighted_count(.x, .y, 2000))
-         )
+    weighted_count_2000 = map2_dbl(varname, value, ~ get_weighted_count(.x, .y, 2000)),
+    weighted_count_2019 = map2_dbl(varname, value, ~ get_weighted_count(.x, .y, 2019))
+    )
+
+pop_2000 <- ipums_db |> filter(YEAR == 2000, GQ %in% c(0, 1, 2)) |> 
+  summarize(weighted_count = sum(PERWT), na.rm = TRUE) |>
+  collect() |>
+  pull(weighted_count)
+  
+pop_2019 <- ipums_db |> filter(YEAR == 2019, GQ %in% c(0, 1, 2)) |> 
+  summarize(weighted_count = sum(PERWT), na.rm = TRUE) |>
+  collect() |>
+  pull(weighted_count)
+
+coef <- coef |>
+  mutate(
+    prop_2000 = weighted_count_2000 / pop_2000,
+    prop_2019 = weighted_count_2019 / pop_2019
+  )
